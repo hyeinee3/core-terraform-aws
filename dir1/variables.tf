bdb4cbf54e242f8bcfd1b8eb9e3c5e3634e7b404
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-10-27 15:48:00 +0900

    Add dir1, dir2

---
+++ dir1/variables.tf
@@ -0,0 +1,31 @@
+ variable "ec2_name" {
+   description = "ec2 instance tag Name"
+   default = "DevOS_terraform_by hyein (Core) - dir1"
+ }
+
+ variable "subnet_id" {
+   description = "subnet identity"
+   default = "subnet-1af11756"
+ }
+
+ variable "security_groups" {
+   description = "security group ids"
+   type = list(string)
+   default = ["sg-01f61050e58083d6e"]
+ }
+
+ variable "key_pair" {
+   description = "pem key pair name"
+   default = "hyein"
+ }
+
+ variable "instance_type" {
+   description = "ec2 instance type"
+   default = "t2.micro"
+ }
+
+ variable "assign_ip" {
+   description = "use assign ip"
+   type = bool
+   default = true
+ }
